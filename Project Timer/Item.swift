33691115b79c4599c01dd3f32cbc5e4a8bb9f8a6
Moritz Wendt <moritz_wendt@icloud.com>
2026-03-27 17:35:13 +0100

    Initial Commit

---
+++ Project Timer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Project Timer
+//
+//  Created by Moritz Wendt on 27.03.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Project TimerTests/Project_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_TimerTests.swift
+//  Project TimerTests
+//
+//  Created by Moritz Wendt on 27.03.26.
+//
+
+import Testing
+@testable import Project_Timer
+
+struct Project_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
